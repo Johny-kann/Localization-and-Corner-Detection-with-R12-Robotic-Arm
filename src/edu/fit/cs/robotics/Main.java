de4c02850f101ad7fa68a210f7b0b96367bb8904
Sridhar Reddy <redreddys2014@my.fit.edu>
2016-02-10 21:11:38 -0500

    Intital Commit. Added .gitignore and a test class to test whether opencv installed on your system and is working

---
+++ src/edu/fit/cs/robotics/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.fit.cs.robotics;
+
+/**
+ * @author virus
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
