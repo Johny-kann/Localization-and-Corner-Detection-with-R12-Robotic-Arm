d72d550f6a4a213d25b15140b7dbe4139e2b411c
Sridhar Reddy <redreddys2014@my.fit.edu>
2016-02-10 21:18:13 -0500

    added comments to add opencv in the project

--- src/edu/fit/cs/robotics/TestOpenCV.java
+++ src/edu/fit/cs/robotics/TestOpenCV.java
@@ -10,6 +10,12 @@ import org.opencv.core.Mat;
 /**
  * @author virus
  *
+ * This is a test class which test whether opencv installed or not. 
+ * Before you run this class, make sure you added opencv in user libraries 
+ * and included in the project. 
+ * 
+ * For more information please follow the instructions from
+ * {@link http://docs.opencv.org/2.4/doc/tutorials/introduction/java_eclipse/java_eclipse.html#java-eclipse} 
  */
 public class TestOpenCV {
 
