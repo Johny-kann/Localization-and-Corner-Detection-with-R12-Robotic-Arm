44203734563ff05b120fa8a85e89b9346aa1feb2
Johny-kann <johny.manasee@gmail.com>
2016-02-25 21:19:08 -0500

    creating fxmls

---
+++ src/main/java/edu/fit/cs/robotics/controller/gui/JmaController.java
@@ -0,0 +1,5 @@
+package edu.fit.cs.robotics.controller.gui;
+
+public class JmaController {
+
+}
---
+++ src/main/java/edu/fit/cs/robotics/controller/gui/MoveToController.java
@@ -0,0 +1,45 @@
+package edu.fit.cs.robotics.controller.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class MoveToController {
+
+	@FXML
+    private Button left;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private TextField step;
+
+    @FXML
+    private Button front;
+
+    @FXML
+    private Button right;
+
+    @FXML
+    private Button up;
+
+    @FXML
+    private Button down;
+
+    @FXML
+    private Button twist;
+
+    @FXML
+    private Button wrist;
+
+    @FXML
+    private Button home;
+    
+    @FXML
+    void initialize()
+    {
+    	
+    }
+    
+}
