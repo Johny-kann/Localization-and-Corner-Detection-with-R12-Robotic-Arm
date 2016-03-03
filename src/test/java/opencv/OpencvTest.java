1b0e0691693d4b8e0d0c7c27a1dcade24b20182c
Johny-kann <johny.manasee@gmail.com>
2016-03-03 16:53:43 -0500

    Points

---
+++ src/test/java/opencv/OpencvTest.java
@@ -0,0 +1,61 @@
+package opencv;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageReader;
+
+import org.aspectj.bridge.IProgressListener;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Range;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
+import javafx.scene.image.Image;
+
+public class OpencvTest {
+
+	
+	public Image imageFX(Mat mat)
+	{
+		MatOfByte byteMat = new MatOfByte();
+		Highgui.imencode(".bmp", mat, byteMat);
+		return new Image(new ByteArrayInputStream(byteMat.toArray()));
+	}
+	
+	public static void main1(String argv[])
+	{
+		Map<String,String> map = new HashMap<String,String>();
+		
+		for(int i=0;i<10;i++)
+		map.put("Slot"+i, i+"th person");
+
+		System.out.println(map.get("Slot5"));
+		
+	}
+	
+	public static void main(String argv[])
+	{
+		nu.pattern.OpenCV.loadShared();
+			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+			Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+			System.out.println("mat = "+mat.dump());
+			
+			Mat m = Highgui.imread("Images/Camera1.png");
+			int type = BufferedImage.TYPE_BYTE_GRAY;
+			if ( m.channels() > 1 ) {
+			    Mat m2 = new Mat();
+			    Imgproc.cvtColor(m,m2,Imgproc.COLOR_BGR2RGB);
+			    type = BufferedImage.TYPE_3BYTE_BGR;
+			    m = m2;
+			}
+			byte [] b = new byte[m.channels()*m.cols()*m.rows()];
+			
+			System.out.println(m.size());
+	}
+}
--- src/test/java/tester/TestGUI.java
+++ src/test/java/tester/TestGUI.java
@@ -1,6 +1,21 @@
 package tester;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 import edu.fit.cs.robotics.BO.RobotLogics;
 import edu.fit.cs.robotics.constants.FXMLConstants;
@@ -15,8 +30,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import opencv.OpencvTest;
 
 public class TestGUI extends Application {
 
@@ -25,7 +42,7 @@ public class TestGUI extends Application {
 		try {
 			
 				
-			FXMLLoader loader = new FXMLLoader();
+/*			FXMLLoader loader = new FXMLLoader();
 			
 			loader.setBuilderFactory(new JavaFXBuilderFactory());
 			
@@ -36,10 +53,18 @@ public class TestGUI extends Application {
 			fxmlLoader.setLocation(location);
 			
 			fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
+*/
 			
-			StackPane root2 =  fxmlLoader.load(location.openStream());
+			StackPane root2 =  new StackPane();
+			
+			ImageView view = new ImageView();
+			view.setFitHeight(600);
+			view.setPreserveRatio(true);
+			
 	
-			ImageShowerController test = fxmlLoader.getController();
+			root2.getChildren().setAll(view);
+			
+	//		ImageShowerController test = fxmlLoader.getController();
 			
 			
 		
@@ -51,32 +76,53 @@ public class TestGUI extends Application {
 			
 			primaryStage.show();
 			
-	/*		ImageService serviceImage = new ImageService();
-			
-			serviceImage.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+//			nu.pattern.OpenCV.loadShared();
+//			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-				@Override
-				public void handle(WorkerStateEvent event) {
-					// TODO Auto-generated method stub
-					System.out.println("Succeded");
-					serviceImage.reset();
+		
+			
+			
+			Mat m = Highgui.imread("Images/Noon_AJMA2.png",Highgui.CV_LOAD_IMAGE_COLOR);
+	//		System.out.println(m.channels());
+			
+/*			MatOfByte byteMat = new MatOfByte();
+			Highgui.imencode(".bmp", m, byteMat);
+			test.addImages(new Image(new ByteArrayInputStream(byteMat.toArray())));
+			
+	*/
+			Mat kernel = new Mat(3,3, CvType.CV_32F){
+	            {
+	               put(0,0,0);
+	               put(0,1,1);
+	               put(0,2,0);
+
+	               put(1,0,1);
+	               put(1,1,-4);
+	               put(1,2,1);
+
+	               put(2,0,0);
+	               put(2,1,1);
+	               put(2,2,0);
+	            }
+	         };
+			
+			 Mat k = m.clone();
+	//		 m.copyTo(k);
+			 
+			 
+			 
+			Imgproc.filter2D(m, k, m.depth(), kernel);
+			
 					
-					Navigator.imageControl.addImages(serviceImage.getValue());
-					
-				}
-			});
+		/*			(Mat_<char>(3,3) <<  0, -1,  0,
+                    -1,  5, -1,
+                     0, -1,  0);
+			*/
 			
 			
-			serviceImage.setUrl("http://debatedecide.fit.edu/robot/4.bmp");
+			view.setImage(new OpencvTest().imageFX(k)
+					);
 			
-			serviceImage.start();
-			
-		*/	
-			
-	/*		test.addImages(RobotLogics.readImageFromFile("Images/Camera1.png"));
-			test.addImages(RobotLogics.readImageFromFile("Images/Camera2.png"));
-			test.addImages(RobotLogics.readImageFromFile("Images/Camera3.png"));
-		*/	
 			
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -84,6 +130,10 @@ public class TestGUI extends Application {
 	}
 
 	public static void main(String[] args) {
+		
+		nu.pattern.OpenCV.loadShared();
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	
 		launch(args);
 	}
 }
